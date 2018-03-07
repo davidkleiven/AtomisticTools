5c52639987176de557e8d90ad44562b42a9ff752
David Kleiven <david.kleiven@ntnu.no>
2018-03-07 17:12:12 +0100

    Several changes

--- atomtools/ce/phonon_ce_eval.py
+++ atomtools/ce/phonon_ce_eval.py
@@ -1,5 +1,6 @@
 try:
     from ase.ce import evaluate as ev
+    from ase.ce.corrFunc import CorrFunction
 except ImportError as exc:
     print ( str(exc) )
     print ("Warning! Cannot use the phonon_ce_eval functionality without ASE version that has Cluster Expansion!")
@@ -10,6 +11,73 @@ import numpy as np
 from ase.units import kB
 from matplotlib import pyplot as plt
 from scipy.integrate import simps
+from atomtools.eos.birch_murnagan import BirschMurnagan
+from ase.db import connect
+from matplotlib import pyplot as plt
+
+class PhononEvalEOS( ev.Evaluate ):
+    def __init__(self, BC, phonon_dbname, cluster_names=None, select_cond=None, lamb=0.0, penalty=None ):
+        self.ph_db = connect( phonon_dbname )
+        self._temperature = 600
+        self.volume = {}
+        self.energy = {}
+        self.atoms_count = {}
+        self.tot_number_of_atoms = {}
+        self.BC = BC
+        ev.Evaluate.__init__( self, BC, cluster_names=cluster_names, lamb=float(lamb), penalty=penalty )
+
+    def _make_cf_matrix( self ):
+        """
+        Override the CF matrix
+        """
+        cf_matrix = []
+        cf = CorrFunction( self.BC )
+        for row in self.ph_db.select():
+            gid = row.key_value_pairs["groupID"]
+            if ( gid in self.volume.keys() ):
+                self.volume[gid].append( row.volume )
+                self.energy[gid].append( row.energy )
+            else:
+                self.volume[gid] = [row.volume]
+                self.energy[gid] = [row.energy]
+                self.atoms_count[gid] = row.count_atoms()
+                self.tot_number_of_atoms[gid] = row.natoms
+                atoms = self.ph_db.get_atoms(id=row.id)
+                if ( len(atoms) == 1 ):
+                    atoms = atoms*(4,4,4)
+                elif ( len(atoms) == 8 ):
+                    atoms = atoms*(2,2,2)
+                else:
+                    raise ValueError( "Can only handle cases where the atoms object contains 1 or 8 atoms!" )
+                atoms.set_cell( self.BC.atoms.get_cell(), scale_atoms=True )
+                corr_funcs = cf.get_cf_by_cluster_names( atoms, self.cluster_names )
+                cf_matrix.append( [corr_funcs[x] for x in self.cluster_names] )
+        return np.array( cf_matrix, dtype=float )
+
+    def _get_dft_energy_per_atom(self):
+        """
+        Override the function in the main class to return the Free Energy associated
+        to elastic and vibrational energy
+        """
+        e_dft = []
+        for key in self.atoms_count.keys():
+            eos = BirschMurnagan( np.array(self.volume[key]), np.array(self.energy[key]) )
+            eos.set_average_mass( self.atoms_count[key] )
+            fvib_el = eos.beta_elastic_vib_free_energy( [self.temperature], natoms=self.tot_number_of_atoms[key] )
+            e_dft.append(fvib_el)
+        self.e_dft = np.array( e_dft )
+        self.e_dft -= np.min(self.e_dft)
+        return True
+
+    @property
+    def temperature(self):
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self,T):
+        self.e_dft = None # Set the e_dft to None as they have to be computed again when the temperature change
+        self.eci = None
+        self._temperature = T
 
 class PhononEval( ev.Evaluate ):
     def __init__(self, BC, cluster_names=None, select_cond=None, lamb=0.0, penalty=None, filters=[], hte=True ):
--- atomtools/eos/equation_of_state.py
+++ atomtools/eos/equation_of_state.py
@@ -1,9 +1,14 @@
 from matplotlib import pyplot as plt
+import numpy as np
+from ase.units import kB, _hbar, _c, eV
+from scipy.optimize import minimize
+from ase.data import atomic_masses, atomic_numbers
 
 class EquationOfState(object):
     def __init__( self, volume, energy ):
         self.volume = volume
         self.energy = energy
+        self.avg_mass = None
 
     def evaluate( self, V ):
         raise NotImplementedError( "This function has to be implemented in subclasses" )
@@ -20,10 +25,10 @@ class EquationOfState(object):
         """
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
-        ax.plot( self.volume, self.energy, "o", mfc="none" )
         V = np.linspace( 0.95*np.min(self.volume), 1.05*np.max(self.volume), 100 )
         fitted = self.evaluate(V)
         ax.plot( V, fitted )
+        ax.plot( self.volume, self.energy, "o", mfc="none" )
         if ( latex ):
             ax.set_xlabel( "Volume (\$\SI{}{\\angstrom^3}\$)")
         else:
@@ -37,5 +42,116 @@ class EquationOfState(object):
         """
         Computes the bulk modulus as a function of volume
         """
-        B = -self.double_deriv(V)/V
+        B = self.double_deriv(V)*V
+        if ( isinstance(B,np.ndarray) ):
+            B[B<0.0] = 0.0
         return B
+
+    def density( self, V ):
+        """
+        Compute the density based on the reference density
+        """
+        if ( self.avg_mass is None ):
+            raise ValueError( "Average mass is not known" )
+        rho = self.avg_mass/V
+        return rho
+
+    def set_average_mass( self, atoms ):
+        """
+        Computes the density given a number dictionary of atoms
+        """
+        n_tot = 0
+        for key,value in atoms.iteritems():
+            n_tot += value
+        avg_mass = 0.0
+        for key,value in atoms.iteritems():
+            avg_mass += value*atomic_masses[atomic_numbers[key]]
+        self.avg_mass = avg_mass/n_tot
+
+    def debye_frequency( self, V ):
+        """
+        Compute the Debye Frequency in rad/s of a given assuming isotropic speed of sound
+        """
+        B = self.bulk_modulus(V)
+        rho = self.density(V)
+        u =  931.4941E6 # eV/c^2
+        rho *= u
+        omega_D = (6.0*np.pi**2)**(1.0/3.0) * V**(-1.0/3.0) * np.sqrt( B/rho )
+        omega_D *= _c*1E10*_hbar
+        eV_si = 1.6021766208E-19 # eV/J
+        omega_D /= eV_si
+        return omega_D # In eV
+
+    def phonon_free_energy_high_temp( self, debye_freq, T ):
+        """
+        Computes the phonon free energy in the high temperature limit
+        """
+        return kB*T*( 3.0*np.log(debye_freq/(kB*T)) - 1.0 )
+
+    def minimum_energy( self ):
+        """
+        Compute the minimum energy
+        """
+        indx = np.argmin(self.energy)
+        V0 = self.volume[indx]
+        res = minimize( self.evaluate, V0 )
+        return res["fun"]
+
+    def beta_elastic_vib_free_energy( self, T, vol_curve=None, natoms=1 ):
+        """
+        Compute the total free energy elastic + vibration
+        """
+        if ( vol_curve is None ):
+            vol_curve = self.volume_temperature( T, natoms )
+
+        fvib = []
+        elastic = self.evaluate( vol_curve )/natoms
+        for V,temp in zip(vol_curve,T):
+            debye = self.debye_frequency(V)
+            fvib.append( self.phonon_free_energy_high_temp(debye,temp) )
+
+        Emin = self.minimum_energy()/natoms
+        E = elastic-Emin + np.array(fvib)
+        if ( len(T) == 1 ):
+            return E[0]/(kB*T[0])
+        return E/(kB*np.array(T))
+
+    def volume_temperature( self, T, natoms ):
+        """
+        Computes the volume as a function of temperature by minimizing the
+        Free Energy of elastic + vibration
+        """
+        min_indx = np.argmin(self.energy)
+        V0 = self.volume[min_indx]
+        volumes = []
+        for temp in T:
+            res = minimize( minization_vol_temp_curve, V0, args=(self,natoms,temp) )
+            volumes.append(res["x"][0])
+            V0 = res["x"]
+        return np.array(volumes)
+
+    def linear_thermal_expansion_coefficient( self, T, natoms=1, vol_curve=None ):
+        """
+        Computes the linear thermal expansion coefficient.
+
+        Relation between linear (alpha_L) and volume (alpha_V) expansion
+        alpha_V = 3*alpha_L
+        """
+        if ( vol_curve is None ):
+            # Volume curve not given so we have to compute it
+            vol_curve = self.volume_temperature( T, natoms )
+
+        X = np.zeros((len(T),3))
+        X[:,0] = 1.0
+        X[:,1] = T
+        X[:,2] = T**2
+        res, residuals,rank,s = np.linalg.lstsq( X, vol_curve )
+        alpha_V = (res[1]+2.0*res[2]*T)/vol_curve
+        alpha_L = alpha_V/3.0
+        return alpha_L
+
+def minization_vol_temp_curve( V, eos, natoms, temperature ):
+    elastic_energy = eos.evaluate(V)/natoms
+    debye_freq = eos.debye_frequency( V )
+    F_vib = eos.phonon_free_energy_high_temp( debye_freq, temperature )
+    return elastic_energy + F_vib
