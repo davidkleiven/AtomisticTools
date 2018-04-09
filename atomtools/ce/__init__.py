162d6455f62799193c873b26865ac7c062f443f6
David Kleiven <david.kleiven@ntnu.no>
2018-04-09 10:45:39 +0200

    Small change

--- atomtools/ce/__init__.py
+++ atomtools/ce/__init__.py
@@ -1,6 +1,6 @@
-#Empty file
 from eciplotter import ECIPlotter
 from population_variance import PopulationVariance
 from phonon_ce_eval import PhononEvalEOS
+from evaluate_gaussian_eof_prior import EvaluateGaussianEOFPrior
 
 __all__ = ["ECIPlotter","PopulationVariance","PhononEvalEOS"]
