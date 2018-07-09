f79b987bd893426d43e970ecc53c42c5d4f3ae6e
David Kleiven <david.kleiven@ntnu.no>
2018-07-09 15:57:03 +0200

    Adapt test to new format

--- tests/test_elastic_constants.py
+++ tests/test_elastic_constants.py
@@ -4,6 +4,7 @@ from ase.build import bulk
 from atomtools.ase import ElasticConstants
 from ase.calculators.calculator import Calculator
 import numpy as np
+import os
 
 # Implement dymmy calculator that supports get_stress
 class DummyCalc(Calculator):
@@ -14,6 +15,7 @@ class DummyCalc(Calculator):
             "stress":np.random.rand(6)
         }
 
+db_name = "test_elastic.db"
 class TestElasticConstante(unittest.TestCase):
     def test_no_throw(self):
         no_throw = True
@@ -22,11 +24,15 @@ class TestElasticConstante(unittest.TestCase):
             atoms = bulk("Al")
             calc = DummyCalc()
             atoms.set_calculator(calc)
-            el = ElasticConstants(atoms)
+            el = ElasticConstants(atoms, db_name)
+            el.prepare_db()
+            for uid in range(1, 25):
+                el.run(uid, calc)
             C = el.get()
             B = el.bulk_modulus()
             G = el.shear_modulus()
             poisson = el.poisson_ratio()
+            os.remove(db_name)
         except Exception as exc:
            msg = str(exc)
            no_throw = False
