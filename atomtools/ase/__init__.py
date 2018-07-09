f0eda7127fee218d843c64d47b6c6805c9b4b438
David Kleiven <david.kleiven@ntnu.no>
2018-07-09 15:28:01 +0200

    Import elastic constant class

--- atomtools/ase/__init__.py
+++ atomtools/ase/__init__.py
@@ -4,3 +4,4 @@ from update_db_info import UpdateDBInfo
 from delete_vacancies import delete_vacancies
 from rotate_atoms_and_cell import rotate_atoms_and_cell, rotate_crd_system, align_direction_with_z
 from constituent_strain import ConstituentStrain
+from atomtools.ase.elastic_constants import ElasticConstants
