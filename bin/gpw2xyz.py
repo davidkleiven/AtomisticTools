cce0e333aaae781705888958b6812096f03e3c3c
David Kleiven <david.kleiven@ntnu.no>
2018-08-16 09:15:32 +0200

    Script to convert all gpw files to xyz

---
+++ bin/gpw2xyz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import glob
+import sys
+from ase.io import write
+import GPAW as gp
+
+
+def main(folder):
+    for fname in glob.glob(folder+"/*.gpw"):
+        atoms, calc = gp.restart(fname)
+        out_fname = fname.rsplit(".")[0]+".xyz"
+        write(out_fname, atoms)
+        print("Saved atoms to {}".format(out_fname))
+
+
+if __name__ == "__main__":
+    folder = sys.argv[1]
+    main(folder)
