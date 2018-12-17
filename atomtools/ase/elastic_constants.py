e2395dbd0c7fbbb6d34c9a8f73831099e70e3a1f
David Kleiven <davidkleiven446@gmail.com>
2018-12-17 10:41:05 +0100

    Change depricated Ruled Surface to Surface

--- atomtools/ase/elastic_constants.py
+++ atomtools/ase/elastic_constants.py
@@ -148,7 +148,8 @@ class ElasticConstants(object):
         db.write(atoms, data={"stress": stress, "strain": strain},
                  key_value_pairs=kvp)
 
-    def get(self, select_cond=[], strains=None, stresses=None, spg=1, perm="xyz"):
+    def get(self, select_cond=[], strains=None, stresses=None, spg=1, perm="xyz",
+            convert_stress_to_mandel=True, convert_strain_to_mandel=False):
         """Compute the elastic properties."""
         if strains is not None and stresses is not None:
             for eps, sigma in zip(strains, stresses):
@@ -171,10 +172,19 @@ class ElasticConstants(object):
             stress_matrix[:, i] = d["stress"]
             strain_matrix[:, i] = d["strain"]
 
-        #self.elastic_tensor = stress_matrix.dot(np.linalg.pinv(strain_matrix))
-        # self.elastic_tensor = np.linalg.lstsq(strain_matrix, stress_matrix)
+        # Convert to Mandel stress (strain is already assumed to be
+        # in mandel representation)
+        if convert_stress_to_mandel:
+            stress_matrix[3:, :] *= np.sqrt(2.0)
+
+        if convert_strain_to_mandel:
+            strain_matrix[3:, :] *= np.sqrt(2.0)
+        print("here")
+        print(stress_matrix)
+        print("here")
         prec = np.linalg.inv(strain_matrix.dot(strain_matrix.T))
-        self.elastic_tensor = prec.dot(stress_matrix.dot(strain_matrix.T))
+        #self.elastic_tensor = prec.dot(stress_matrix.dot(strain_matrix.T))
+        self.elastic_tensor = stress_matrix.dot(strain_matrix.T).dot(prec)
         self._symmetrize_elastic_tensor(spg=spg, perm=perm)
         return self.elastic_tensor
 
@@ -209,26 +219,32 @@ class ElasticConstants(object):
             msg = "Elastic tensor not computed."
             msg += "Call get() method first."
             raise ValueError(msg)
-        return np.linalg.inv(self.elastic_tensor)
+        
+        # Convert Mandel tensor into Voigt tensor
+        C = self.elastic_tensor.copy()
+        C[3:, :3] /= np.sqrt(2.0)
+        C[:3, 3:] /= np.sqrt(2.0)
+        C[3:, 3:] /= 2.0
+        return np.linalg.inv(C)
 
     @property
     def isotropic_elastic_tensor(self):
         """Return the isotropic elastic tensor.
         Uses the bulk modulus and the poisson ratio obtained from the
-        general tensor
+        general tensor (in Mandel notation)
         """
-        K = self._bulk_mod_mandel()
-        mu = self.poisson_ratio
+        K = self._bulk_mod_voigt()
+        mu = self._shear_mod_voigt()
         tensor = np.zeros((6, 6))
         tensor[0, 0] = tensor[1, 1] = tensor[2, 2] = K + 4.0*mu/3.0
-        tensor[3, 3] = tensor[4, 4] = tensor[5, 5] = mu
+        tensor[3, 3] = tensor[4, 4] = tensor[5, 5] = 2*mu
         tensor[0, 1] = tensor[0, 2] = K - 2.0 * mu/3.0
         tensor[1, 0] = tensor[1, 2] = K - 2.0 * mu/3.0
         tensor[2, 0] = tensor[2, 1] = K - 2.0 * mu/3.0
         return tensor
 
-    def _bulk_mod_mandel(self):
-        """Bulk modulus by the mandel average."""
+    def _bulk_mod_voigt(self):
+        """Bulk modulus by the voigt average."""
         if self.elastic_tensor is None:
             msg = "Elastic tensor not computed."
             msg += "Call get() method first."
@@ -247,8 +263,8 @@ class ElasticConstants(object):
         kR = 1.0/inv_KR
         return kR
 
-    def _shear_mod_mandel(self):
-        """Shear modulus by mandel average."""
+    def _shear_mod_voigt(self):
+        """Shear modulus by voigt average."""
         if self.elastic_tensor is None:
             msg = "Elastic tensor not computed."
             msg += "Call get() method first."
@@ -256,7 +272,7 @@ class ElasticConstants(object):
         C = self.elastic_tensor
         Gv = C[0, 0] + C[1, 1] + C[2, 2]
         Gv -= (C[0, 1] + C[1, 2] + C[2, 0])
-        Gv += 3.0*(C[3, 3] + C[4, 4] + C[5, 5])
+        Gv += 3.0*(C[3, 3] + C[4, 4] + C[5, 5])*0.5
         return Gv/15.0
 
     def _shear_mod_reuss(self):
@@ -275,11 +291,11 @@ class ElasticConstants(object):
             raise ValueError("Mode has to be one of {}".format(allowed_modes))
 
         if mode == "V":
-            return self._shear_mod_mandel()
+            return self._shear_mod_voigt()
         if mode == "R":
             return self._shear_mod_reuss()
 
-        Gv = self._shear_mod_mandel()
+        Gv = self._shear_mod_voigt()
         Gr = self._shear_mod_reuss()
         return 0.5*(Gv + Gr)
 
@@ -289,11 +305,11 @@ class ElasticConstants(object):
         if mode not in allowed_modes:
             raise ValueError("Mode has to be one of {}".format(allowed_modes))
         if mode == "V":
-            return self._bulk_mod_mandel()
+            return self._bulk_mod_voigt()
         elif mode == "R":
             return self._bulk_mode_reuss()
 
-        Kv = self._bulk_mod_mandel()
+        Kv = self._bulk_mod_voigt()
         Kr = self._bulk_mode_reuss()
         return 0.5*(Kv + Kr)
 
